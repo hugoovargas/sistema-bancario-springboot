12049849676d659a2e8730a368979f6862eab776
hugoovargas <hugoovvargas@gmail.com>
2026-08-05 15:33:39 -0300

    feat: expand repository and domain capabilities

--- src/main/java/com/banco/bank_system/infrastructure/database/entities/ClientEntity.java
+++ src/main/java/com/banco/bank_system/infrastructure/database/entities/ClientEntity.java
@@ -1,22 +1,30 @@
 package com.banco.bank_system.infrastructure.database.entities;
 
+
 import com.banco.bank_system.domain.entities.Client;
 import com.banco.bank_system.domain.valueobject.CPF;
 import com.banco.bank_system.domain.valueobject.Email;
 import com.banco.bank_system.domain.valueobject.PersonName;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
+
+
+import java.util.UUID;
+
 
 @Entity
 @Table(name = "tb_clients")
 @Getter
-@Setter
 @NoArgsConstructor
 public class ClientEntity {
+
+    @Id
+    @Column(
+            nullable = false,
+            updatable = false
+    )
+    private UUID id;
 
     @Column(nullable = false, unique = true)
     private String email;
@@ -27,18 +35,37 @@ public class ClientEntity {
     @Column(nullable = false, unique = true)
     private String cpf;
 
-    public ClientEntity(String name, String cpf, String email) {
+    public ClientEntity(
+            UUID id,
+            String name,
+            String cpf,
+            String email
+    ){
+
+        this.id = id;
         this.name = name;
         this.cpf = cpf;
         this.email = email;
     }
 
-    public Client toDomain() {
-        return new Client(new PersonName(name), new CPF(cpf), new Email(this.email));
+
+
+    public Client toDomain(){
+
+        return new Client(
+                id,
+                new PersonName(name),
+                new CPF(cpf),
+                new Email(email)
+        );
     }
 
-    public static ClientEntity fromDomain(Client client) {
+
+
+    public static ClientEntity fromDomain(Client client){
+
         return new ClientEntity(
+                client.getId(),
                 client.getName().value(),
                 client.getCpf().value(),
                 client.getEmail().value()
